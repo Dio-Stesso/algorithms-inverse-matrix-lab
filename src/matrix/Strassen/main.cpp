e100f0145bd672e5cb3240d2b45a3479a8523679
Yulia Petryk <98975815+yuliapetryk@users.noreply.github.com>
2022-10-24 22:07:06 +0300

    Create main.cpp

---
+++ src/matrix/Strassen/main.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+#include <iostream>
+#include "Basic_functions.h"
+#include "Strassen.h"
+
+using namespace std;
+
+int main() {
+	Matrix a = { {1,1,1,1,1,1,1},{2,3,4.5,4.5,1,1,1}, {2,3,1,12.4,1,1,1},{2,2.3,5,3.4,1,1,1} ,{1,1,1,1,1,1,1} };
+	Matrix b = { {1,1},{2,4},{2,2},{3.5,3.4},{1,1},{1,1},{1,1} };
+	Matrix result;
+	result = Strassen_multiplication(a, b);
+	print_matrix(result);
+}
