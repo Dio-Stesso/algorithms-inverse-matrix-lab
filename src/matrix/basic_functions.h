73ac5800ee6b3e7fa694af5cafb51e4e17059349
Yulia Petryk <98975815+yuliapetryk@users.noreply.github.com>
2022-11-06 20:46:46 +0200

    Add class

--- src/matrix/basic_functions.h
+++ src/matrix/basic_functions.h
@@ -4,96 +4,146 @@
 #include <iostream>
 
 using namespace std;
+class Real_valued {
+public:
 
-vector<vector<float>> typedef Matrix;
+	vector<vector<double>>  Matrix;
+	int rows; //= matrix.size()
+	int columns;// matrix[0].size()
 
-void print_matrix(Matrix matrix) {
-	int m = (matrix).size();
-	int n = (matrix)[0].size();
-	for (int i = 0; i < m; i++)
-	{
-		for (int j = 0; j < n; j++) {
-            cout << matrix[i][j] << " ";
-
+	bool compare (Real_valued matrix) {
+		if ((this->Matrix.size() != matrix.Matrix.size()) || (this->Matrix[0].size() != matrix.Matrix[0].size())) {
+			return false;
 		}
-		cout << endl;
-	}
-}
-
-bool correct_size_for_multiplication(const Matrix& first, const Matrix& second)
-{
-	if (first[0].size() != second.size()) {
-		return false;
-	}
-	return true;
-}
-
-bool correct_sizes(const Matrix& first, const Matrix& second)
-{
-	if (first.size() != second.size() || first[0].size() != second[0].size()) {
-		false;
-	}
-	return true;
-}
-
-Matrix operator * (const Matrix& first, const Matrix& second)
-{
-	if (!correct_size_for_multiplication(first, second)) {
-		throw std::runtime_error("");
-	}
-	Matrix result(first.size());
-	for (size_t i = 0; i < first.size(); ++i) {
-		result[i].resize(second[0].size());
-		for (size_t j = 0; j < second[0].size(); ++j) {
-			for (size_t k = 0; k < first[0].size(); ++k) {
-				result[i][j] += first[i][k] * second[k][j];
+		for (int i = 0; i < this->Matrix.size(); i += 1) {
+			for (int j = 0; j < this->Matrix[0].size(); j += 1) {
+				if ( int(this->Matrix[i][j] - matrix.Matrix[i][j])!=0) {
+					return false;
+				}
 			}
 		}
+		return true;
 	}
-	return result;
-}
 
-Matrix operator +(Matrix first, const Matrix& second)
-{
-	if (!correct_sizes(first, second)) {
-		throw std::runtime_error("");
-	}
-	for (size_t i = 0; i < first.size(); ++i) {
-		for (size_t j = 0; j < first[0].size(); ++j) {
-			first[i][j] += second[i][j];
+	void print_matrix() {
+		for (int i = 0; i < this->Matrix.size(); i++)
+		{
+			for (int j = 0; j < this->Matrix[0].size(); j++) {
+				cout << this->Matrix[i][j] << " ";
+
+			}
+			cout << endl;
 		}
 	}
-	return first;
-}
 
-Matrix operator -(Matrix first, const Matrix& second)
-{
-	if (!correct_sizes(first, second)) {
-		throw std::runtime_error("");
+	bool  correct_size_for_multiplication(const Real_valued matrix)
+	{
+		if (this->columns != matrix.rows) {
+			cout << this->columns << " " << matrix.rows << endl;
+			return false;
+		}
+		return true;
 	}
-	for (size_t i = 0; i < first.size(); ++i) {
-		for (size_t j = 0; j < first[0].size(); ++j) {
-			first[i][j] -= second[i][j];
+
+	bool correct_sizes(const Real_valued matrix)
+	{
+		if (this->rows != matrix.rows || this->columns != matrix.columns) {
+			false;
+		}
+		return true;
+	}
+
+	Real_valued multiplication(const Real_valued matrix) {
+		this->rows = this->Matrix.size();
+		this->columns = this->Matrix[0].size();
+		if (!this->correct_size_for_multiplication(matrix)) {
+			throw runtime_error("");
+		}
+		Real_valued  result(this->rows);
+		for (size_t i = 0; i < this->rows; ++i) {
+			result.Matrix[i].resize(matrix.columns);
+			for (size_t j = 0; j < matrix.columns; ++j) {
+				for (size_t k = 0; k < this->columns; ++k) {
+					result.Matrix[i][j] += this->Matrix[i][k] * matrix.Matrix[k][j];
+				}
+			}
+		}
+		return result;
+	}
+
+	Real_valued addition(Real_valued matrix)
+	{
+		if (!this->correct_sizes(matrix)) {
+			throw runtime_error("");
+		}
+		for (size_t i = 0; i < matrix.rows; ++i) {
+			for (size_t j = 0; j < matrix.columns; ++j) {
+				matrix.Matrix[i][j] += this->Matrix[i][j];
+			}
+		}
+		return matrix;
+	}
+
+	Real_valued subtraction (Real_valued matrix)
+	{
+		if (!correct_sizes(matrix)) {
+			throw runtime_error("");
+		}
+		for (size_t i = 0; i < matrix.rows; ++i) {
+			for (size_t j = 0; j < matrix.columns; ++j) {
+				matrix.Matrix[i][j] -= this->Matrix[i][j];
+			}
+		}
+		return matrix;
+	} 
+
+	void expand(const size_t& size)
+	{
+		this->Matrix.resize(size);
+		for (auto& row : this->Matrix) {
+			row.resize(size, 0);
 		}
 	}
-	return first;
-}
-
-void expand(Matrix& matrix, const size_t& size)
-{
-	matrix.resize(size);
-	for (auto& row : matrix) {
-		row.resize(size, 0);
+	
+	//Constructors
+	Real_valued(const int& n) {
+		vector<vector<double>> matrix;
+		matrix.resize(n);
+		for (int i = 0; i < n; i++) {
+			matrix[i].resize(n);
+		}
+		Matrix = matrix;
+		rows = matrix.size();
+		columns = matrix[0].size();
 	}
-}
 
-Matrix transpose(const Matrix &m) {
-    int n = m.size();
-    Matrix r(n, vector<float>(n));
-    for (int i = 0; i < n; i += 1) {
-        for (int k = 0; k < n; k += 1) {
-            r[i][k] = m[k][i];
-        }
-    }
-    return r;
-}
+	Real_valued(vector<vector<double>> matrix ) {
+		Matrix = matrix;
+		rows = matrix.size();
+		columns = matrix[0].size();
+	}
+
+	Real_valued(const int& N, const int& M) {
+		vector<vector<double>> matrix;
+		rows = N;
+		columns = M;
+		for (int i = 0; i < this->rows; ++i) {
+			vector<double> line;
+			for (int j = 0; j < this->columns; ++j) {
+				double number=rand()%1000;
+				line.push_back(number);
+			}
+			matrix.push_back(line);
+		}
+		Matrix = matrix;	
+	}
+	
+	// for Strassen multiplication 
+	void split(Real_valued &a1, Real_valued& a2, Real_valued& a3, Real_valued& a4);
+	void collect(Real_valued& c11, Real_valued & c12, Real_valued& c21, Real_valued& c22);
+	Real_valued Strassens_algorithm(Real_valued matrix, size_t n);
+	Real_valued Strassen_multiplication(Real_valued matrix);
+	auto test_time(const Real_valued& matrix);
+	auto test_time_Strassen(const Real_valued& matrix);
+	void test(const Real_valued& matrix);
+};
